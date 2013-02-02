c6a63bd2b1225879c266e9ed7b143ebe72e983b7
Alex Crawford <adrofwarc@gmail.com>
2013-02-01 17:11:38 -0800

    Cleaning up code

--- main.rb
+++ main.rb
@@ -17,47 +17,33 @@ class ListService < Sinatra::Application
 	end
 
 	get '/lists/:list' do
-		list = List.find_by_name(params[:list])
-		unless list
+		if (list = List.find_by_name(params[:list]))
+			list.to_json
+		else
 			status 404
-			return
 		end
-		list.to_json
 	end
 
 	get '/lists/:list/items' do
-		list = List.find_by_name(params[:list])
-		unless list
+		if (list = List.find_by_name(params[:list]))
+			list.items.to_json
+		else
 			status 404
-			return
 		end
-		list.items.where(:deleted => false).to_json
 	end
 
 	get '/lists/:list/items/:item' do
-		list = List.find_by_name(params[:list])
-		unless list
+		if (list = List.find_by_name(params[:list])) &&
+		   (item = list.items.where(:name => params[:item]).first)
+			item.to_json
+		else
 			status 404
-			return
 		end
-		item = list.items.where(:name => params[:item]).first
-		unless item
-			status 404
-			return
-		end
-		item.to_json
 	end
 
 	put '/lists/:list/items/:item' do
-		list = List.find_by_name(params[:list])
-		unless list
-			list = List.create(:name => params[:list])
-		end
-
-		item = list.items.where(:name => params[:item]).first
-		unless item
-			item = list.items.create(:name => params[:item])
-		end
+		list = List.find_or_create_by_name(params[:list])
+		item = list.items.find_or_create_by_name(params[:item])
 
 		item.deleted = false
 
@@ -72,19 +58,14 @@ class ListService < Sinatra::Application
 	end
 
 	delete '/lists/:list/items/:item' do
-		list = List.find_by_name(params[:list])
-		unless list
+		if (list = List.find_by_name(params[:list])) &&
+		   (item = list.items.where(:name => params[:item]).first)
+			item.deleted = true
+			unless item.save
+				status 500
+			end
+		else
 			status 404
-			return
-		end
-		item = list.items.where(:name => params[:item]).first
-		unless item
-			status 404
-			return
-		end
-		item.deleted = true
-		unless item.save
-			status 500
 		end
 	end
 end
