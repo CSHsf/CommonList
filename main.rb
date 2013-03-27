3531e7c29c51a828103d1113e7191f26c3a6b4dc
Alex Crawford <adrofwarc@gmail.com>
2013-03-27 09:09:27 -0700

    Fixing up endpoints -Using error() to capture 404s

--- main.rb
+++ main.rb
@@ -8,11 +8,12 @@ class ListService < Sinatra::Application
 	end
 
 	helpers do
-		def format(obj)
-			if obj.nil?
-				halt 404
-			else
-				obj.to_json
+		def parse_bool_param(str)
+			case str.try(:downcase)
+				when false.to_s then false
+				when true.to_s  then true
+				when nil        then nil
+				else raise ArgumentError, "'#{str}' is not a bool"
 			end
 		end
 	end
@@ -22,53 +23,50 @@ class ListService < Sinatra::Application
 		content_type 'application/json'
 	end
 
+	error do
+		ex = env['sinatra.error']
+		# TODO: Jumpfrog this error
+		p ex
+		halt 500
+	end
+
+	error ActiveRecord::RecordNotFound do
+		halt 404
+	end
+
 	get '/lists/:list_id' do
-		format List.find_by_id(params[:list_id])
+		List.find_by_id!(params[:list_id]).to_json
 	end
 
 	get '/lists/:list_id/items' do
-		list = List.find_by_id(params[:list_id])
-		format list.nil? ? nil : list.items
+		List.find_by_id!(params[:list_id]).items.to_json
 	end
 
 	get '/lists/:list_id/items/:item' do
-		list = List.find_by_id(params[:list_id])
-		format list.nil? ? nil : list.items.where(:name => params[:item]).first
+		List.find_by_id!(params[:list_id]).items.where(:name => params[:item]).first!.to_json
 	end
 
 	put '/lists/:list_id/items/:item' do
-		needed = case params[:needed].try(:downcase)
-			when false.to_s then false
-			when true.to_s  then true
-			when nil        then nil
-			else halt 500
-		end
+		needed = parse_bool_param(params[:needed])
 		list = List.find_or_create_by_id(params[:list_id])
 		item = list.items.find_or_create_by_name(params[:item])
 
 		list.title = params[:title] if params[:title]
 		item.deleted = false
 
-		unless item.needed == needed || needed.nil?
+		unless item.needed == needed or needed.nil?
 			item.needed = needed
 			item.events.create(:needed => needed, :deleted => false)
 		end
 
-		unless item.save && list.save
-			halt 500
-		end
+		item.save
+		list.save
 	end
 
 	delete '/lists/:list_id/items/:item' do
-		if (list = List.find_by_id(params[:list_id])) &&
-		   (item = list.items.where(:name => params[:item]).first)
-			item.deleted = true
-			item.events.create(:needed => item.needed, :deleted => true)
-			unless item.save
-				halt 500
-			end
-		else
-			status 404
-		end
+		item = List.find_by_id!(params[:list_id]).items.where(:name => params[:item]).first!
+		item.deleted = true
+		item.events.create(:needed => item.needed, :deleted => true)
+		item.save
 	end
 end
